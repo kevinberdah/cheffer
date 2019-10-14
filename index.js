9ec9360bbaf13f238694c90cfb2b2d9fc911f80c
kevinberdah <43068827+kevinberdah@users.noreply.github.com>
2019-10-14 23:43:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+////// start navbar color scroll //////
+
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 20) nav.className = ''; else nav.className = 'scroll';
+  const navv = document.querySelector('#navbarr');
+  if(this.scrollY <= 20) navv.className = ''; else navv.className = 'scroll';
+};
+////// end navbar color scroll //////
+
+
+
+
