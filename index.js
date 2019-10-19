50a5325505fe5a4e865136a1a44593425da793d6
Raphael Dray <dray.raph@gmail.com>
2019-10-19 20:16:40 +0200

    First Commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 ////// start navbar color scroll //////
 
-window.onscroll = () => {
+/* window.onscroll = () => {
   const nav = document.querySelector('#navbar');
   if(this.scrollY <= 20) nav.className = ''; else nav.className = 'scroll';
   const navv = document.querySelector('#navbarr');
   if(this.scrollY <= 20) navv.className = ''; else navv.className = 'scroll';
 };
-////// end navbar color scroll //////
+ */////// end navbar color scroll //////
 
 
 
