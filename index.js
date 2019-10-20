a8d3ae2239fccfe707ced206beeca66d8766a25a
Raphael Dray <dray.raph@gmail.com>
2019-10-20 12:35:51 +0200

    Refactoring: Whole Website

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-////// start navbar color scroll //////
-
-/* window.onscroll = () => {
-  const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 20) nav.className = ''; else nav.className = 'scroll';
-  const navv = document.querySelector('#navbarr');
-  if(this.scrollY <= 20) navv.className = ''; else navv.className = 'scroll';
-};
- */////// end navbar color scroll //////
-
-
+const selectElement = (element) => document.querySelector(element);
+//Open and close nav on click
+selectElement('.menu-icons').addEventListener('click', () => {
+  selectElement('nav').classList.toggle('active');
+})
 
 
