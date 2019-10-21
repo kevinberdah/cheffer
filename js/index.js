7d7a45360725bade79b9b51dbce85e5c374eb53d
Raphael Dray <dray.raph@gmail.com>
2019-10-21 20:08:01 +0200

    Add: Search Bar in Hero

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const selectElement = (element) => document.querySelector(element);
 //Open and close nav on click
 selectElement('.menu-icons').addEventListener('click', () => {
   selectElement('nav').classList.toggle('active');
-})
+});
 
+/*const choices = new Choices('[data-trigger]', {
+  searchEnabled: false,
+  itemSelectText: '',
+});*/
 
