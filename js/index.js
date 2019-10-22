b74c8666cc2417667170e2ab790ece1395914c4a
Raphael Dray <dray.raph@gmail.com>
2019-10-22 14:47:17 +0200

    Add: Changing Background dynamicaly

--- js/index.js
+++ js/index.js
@@ -4,8 +4,22 @@ selectElement('.menu-icons').addEventListener('click', () => {
   selectElement('nav').classList.toggle('active');
 });
 
-/*const choices = new Choices('[data-trigger]', {
-  searchEnabled: false,
-  itemSelectText: '',
-});*/
+const images = [
+  'bg.jpg',
+  'bg2.jpg',
+  'bg3.jpg',
+  'bg4.jpg'
+];
 
+let i = 0;
+const changeImage = () => {
+  if(++i === images.length) i = 0;
+  selectElement('.hero').style.background = 'linear-gradient(to bottom, rgba(0,0,0,0.8),rgba(0,0,0,0.8)),'+
+                                            ' url(./images/' +
+                                            images[i] + 
+                                            ') center no-repeat';
+}
+
+$(document).ready(() => {
+  setInterval(changeImage, 5000);
+});
